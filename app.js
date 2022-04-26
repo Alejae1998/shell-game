cf5ade830a86ba9b50fd556e60cc72cbeff3d557
Alejae1998 <alejagr1998@gmail.com>
2022-04-25 18:34:54 -0700

    added css and more html

--- app.js
+++ app.js
@@ -1,4 +1,53 @@
 // import functions and grab DOM elements
+const button1 = document.getElementById('button1');
+const shell1 = document.getElementById('shell1'); 
+const winSpan = document.getElementById('wins');
+const lossesSpan = document.getElementById('losses');
+const totalSpan = document.getElementById('total');
+
+let wins = 0;
+let losses = 0;
+
+const button2 = document.getElementById('button2');
+const shell2 = document.getElementById('shell2');   
+
+const button3 = document.getElementById('button3');
+const shell3 = document.getElementById('shell3');
+
+button1.addEventListener('click', () => {
+    shell1.classList.remove('reveal');
+    shell2.classList.remove('reveal');
+    shell3.classList.remove('reveal'); 
+    const randomNumber1 = Math.ceil(Math.random() * 3);
+    if (randomNumber1 === 1){
+        shell1.classList.add('reveal');
+        wins++; 
+    } else if (randomNumber1 === 2){
+        shell2.classList.add('reveal');
+        losses++;
+    } else {
+        shell3.classList.add('reveal');
+        losses++;
+    }
+    
+    winSpan.textContent = wins;
+    lossesSpan.textContent = losses;
+    totalSpan.textContent = wins + losses;
+
+});   
+button2.addEventListener('click', () => {
+    shell1.classList.remove('reveal');
+    shell2.classList.remove('reveal');
+    shell3.classList.remove('reveal');
+});  
+
+button3.addEventListener('click', () => {
+    shell1.classList.remove('reveal');
+    shell2.classList.remove('reveal');
+    shell3.classList.remove('reveal');
+});  
+
+
 
 // let state
 
